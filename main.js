6a72b930a81f90cb418e0fac4874d43a2ea8b469
Amanda <32129847+zhumengyua@users.noreply.github.com>
2019-03-27 12:04:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ var FooterJs = {
   },
   render() {
     var _this = this;
-    $.getJSON('//api.jirengu.com/fm/getChannels.php')
+    $.getJSON('//api.jirengu.com/fm/getChannels.com')
       .done(function (ret) {
         _this.renderFooter(ret.channels)
       })
@@ -181,7 +181,7 @@ var Fm = {
   loadMusic(callback) {
     var _this = this
     console.log('loadMusic..')
-    $.getJSON('//api.jirengu.com/fm/getSong.php', {
+    $.getJSON('//api.jirengu.com/fm/getSong.com', {
       channel: this.channelId
     }).done(function (ret) {
       _this.song = ret['song'][0]
@@ -193,7 +193,8 @@ var Fm = {
   loadLyric() {
     var _this = this
     console.log('loadMusic..')
-    $.getJSON('//api.jirengu.com/fm/getLyric.php', {
+    $.getJSON('//api.jirengu.com/fm/getLyric.com
+              ', {
       sid: this.song.sid
     }).done(function (ret) {
       var lyric = ret.lyric
